24dffa2216348a4cb161885695644e091f4e6941
Sunnykundr123 <141571668+Sunnykundr123@users.noreply.github.com>
2023-08-08 23:15:32 +0530

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -12,8 +12,8 @@ handler.all = async function (m) {
 
 export default handler
 let audioMsg = {
-  'Welcome':'https://s17.aconvert.com/convert/p3r68-cdx67/4nftu-75yik.mp3',
-  'alive':'https://s33.aconvert.com/convert/p3r68-cdx67/cmvrs-f8k1d.mp3',
+  'Welcome': 'https://s17.aconvert.com/convert/p3r68-cdx67/4nftu-75yik.mp3',
+  'alive': 'https://s33.aconvert.com/convert/p3r68-cdx67/cmvrs-f8k1d.mp3',
   'Alfa': 'https://s19.aconvert.com/convert/p3r68-cdx67/l7plh-dip7n.mp3',
   'Amma': 'https://s31.aconvert.com/convert/p3r68-cdx67/s8crv-3ct6q.mp3',
   'Nanban': 'https://s27.aconvert.com/convert/p3r68-cdx67/ux6u5-id19z.mp3',
