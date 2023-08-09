f9750ecb599bf2b3cb0cc9889a82e6b8da115d24
Sunnykundr123 <141571668+Sunnykundr123@users.noreply.github.com>
2023-08-09 07:10:34 +0530

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -8,12 +8,12 @@ handler.all = async function (m) {
     }
   }
   return !0
- }
-
+}
 export default handler
 let audioMsg = {
   'Welcome':'https://s17.aconvert.com/convert/p3r68-cdx67/4nftu-75yik.mp3',
   'alive': 'https://s33.aconvert.com/convert/p3r68-cdx67/cmvrs-f8k1d.mp3',
+  'welcome':'https://s31.aconvert.com/convert/p3r68-cdx67/z5vwl-nospt.mp3',
   'Alfa': 'https://s19.aconvert.com/convert/p3r68-cdx67/l7plh-dip7n.mp3',
   'Amma': 'https://s31.aconvert.com/convert/p3r68-cdx67/s8crv-3ct6q.mp3',
   'Nanban': 'https://s27.aconvert.com/convert/p3r68-cdx67/ux6u5-id19z.mp3',
