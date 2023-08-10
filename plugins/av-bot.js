c727b7ec9bc7878fdbf29bb7567e087b2bf54f76
Sunnykundr123 <141571668+Sunnykundr123@users.noreply.github.com>
2023-08-11 03:15:32 +0530

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -2,9 +2,13 @@ let handler = async (m, { conn}) => {
 
 let name = conn.getName(m.sender)
 let av = `./src/mp3/${pick(["alive"])}.mp3`,
-      let av = `./src/mp3/${pick(["Da"])}.mp3`,
-      let av = `./src/mp3/${pick(["Aa"])}.mp3`
+    
+let name = conn.getName(m.sender)
+let av = `./src/mp3/${pick(["Da"])}.mp3`
 
+let name = conn.getName(m.sender)
+let av = `./src/mp3/${pick(["Aa"])}.mp3`
+      
 conn.sendButton(m.chat, `Hola *${name}* \n \nNecesitas ayuda? \n`, fgig, null, [
       ['⦙☰ Menu', '/help'],
       ['⦙☰ Menu 2', '/menu2'],
