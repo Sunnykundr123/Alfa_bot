ef77fc0fa72e397a0a69e57b44969061dfcc6686
Sunnykundr123 <141571668+Sunnykundr123@users.noreply.github.com>
2023-08-11 00:17:31 +0530

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -1,24 +1,21 @@
 import fs from 'fs';
 
-let handler = async function (m) {
+let handler = async (m) => m;
+handler.all = async function (m) {
   for (const message in audioMsg) {
     if (new RegExp(`^${message}$`, 'i').test(m.text)) {
-      const audioPath = audioMsg[message];
-      if (fs.existsSync(audioPath)) {
-        const audioBuffer = fs.readFileSync(audioPath);
-        this.sendAudio(m.chat, audioBuffer, null, m, true);
-      } else {
-        this.reply(m.chat, 'Audio not found', m);
-      }
+      const audioFilePath = audioMsg[message];
+      const audioBuffer = fs.readFileSync(audioFilePath);
+      this.sendAudio(m.chat, audioBuffer, 'audio.mp3', null, m, true);
       break;
     }
   }
   return true;
-}
+};
 
 export default handler;
 
 let audioMsg = {
   'fino señores': './src/mp3/fino.mp3',
-  // Add more audio mappings here
+  // Add more entries here as needed
 };
