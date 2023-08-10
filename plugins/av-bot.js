fa744de55414738ee5508a8d6cfc16f3ceecea72
Sunnykundr123 <141571668+Sunnykundr123@users.noreply.github.com>
2023-08-11 03:20:20 +0530

    Update av-bot.js

--- plugins/av-bot.js
+++ plugins/av-bot.js
@@ -1,13 +1,8 @@
 let handler = async (m, { conn}) => {
 
 let name = conn.getName(m.sender)
-let av = `./src/mp3/${pick(["alive"])}.mp3`,
-    
-let name = conn.getName(m.sender)
-let av = `./src/mp3/${pick(["Da"])}.mp3`
-
-let name = conn.getName(m.sender)
-let av = `./src/mp3/${pick(["Aa"])}.mp3`
+let av = `./src/mp3/${pick(["alive"])}.mp3`
+    let av = `./src/mp3/${pick(["Da"])}.mp3`
       
 conn.sendButton(m.chat, `Hola *${name}* \n \nNecesitas ayuda? \n`, fgig, null, [
       ['⦙☰ Menu', '/help'],
@@ -19,15 +14,7 @@ conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', pt
 
 handler.customPrefix = /^(alive)$/i
 handler.command = new RegExp
-
-export default handler
-
 handler.customPrefix = /^(Da)$/i
-handler.command = new RegExp
-
-export default handler
-
-handler.customPrefix = /^(Aa)$/i
 handler.command = new RegExp
 
 export default handler
