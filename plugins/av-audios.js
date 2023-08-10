efe2899b8b49525fda2085410fc2e95107a7be63
Sunnykundr123 <141571668+Sunnykundr123@users.noreply.github.com>
2023-08-11 01:22:24 +0530

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -1,21 +1,26 @@
-import media from 'media';
 
-let handler = async (m) => m;
+let handler = m => m
 handler.all = async function (m) {
   for (const message in audioMsg) {
     if (new RegExp(`^${message}$`, 'i').test(m.text)) {
-      const audioFilePath = audioMsg[message];
-      const audioBuffer = fs.readFileSync(audioFilePath);
-      this.sendAudio(m.chat, audioBuffer, 'audio.mp3', null, m, true);
-      break;
+      this.sendFile(m.chat, audioMsg[message], 'audio.mp3', null, m, true)
+      break
     }
   }
-  return true;
-};
+  return !0
+ }
 
-export default handler;
+export default handler
+
 
 let audioMsg = {
-  'fino señores': './media',
-  // Add more entries here as needed
-};
+  'fino señores': './src/mp3/fino.mp3',
+  'alive': 'https://s33.aconvert.com/convert/p3r68-cdx67/cmvrs-f8k1d.mp3',
+  'Alfa': 'https://s19.aconvert.com/convert/p3r68-cdx67/l7plh-dip7n.mp3',
+  'Amma': 'https://s31.aconvert.com/convert/p3r68-cdx67/s8crv-3ct6q.mp3',
+  'Nanban': 'https://s27.aconvert.com/convert/p3r68-cdx67/ux6u5-id19z.mp3',
+  'Bgm': 'https://s19.aconvert.com/convert/p3r68-cdx67/pltnp-d829w.mp3',
+  'Love': 'https://s17.aconvert.com/convert/p3r68-cdx67/5wrpd-lgwhh.mp3',
+  '❤️': 'https://s27.aconvert.com/convert/p3r68-cdx67/drc17-u9rn7.mp3',
+  'bot': 'https://s21.aconvert.com/convert/p3r68-cdx67/unwpb-4rf3l.mp3',
+}
